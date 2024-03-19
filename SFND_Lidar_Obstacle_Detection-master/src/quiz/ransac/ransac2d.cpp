0fd23d994ca78178b33b7ca959521b0fbdcdff0d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-03-18 20:52:51 -0400

    Created RansacPlane function

--- SFND_Lidar_Obstacle_Detection-master/src/quiz/ransac/ransac2d.cpp
+++ SFND_Lidar_Obstacle_Detection-master/src/quiz/ransac/ransac2d.cpp
@@ -130,6 +130,96 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 
 }
 
+std::unordered_set<int> RansacPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;	//holds BEST inliers
+	srand(time(NULL));
+	
+	// TODO: Fill in this function
+
+	// For max iterations 
+
+	// Randomly sample subset and fit line
+
+	// Measure distance between every point and fitted line
+	// If distance is smaller than threshold count it as inlier
+
+	// Return indicies of inliers from fitted line with most inliers
+	
+	while (maxIterations--)		// run for max number of iterations (while "max iterations > 0")
+	{
+		// Randomly pick two points
+
+		std::unordered_set<int> inliers;		// pick unique values ONLY
+		while (inliers.size() < 2)
+		{
+			inliers.insert(rand()%(cloud->points.size()));	// randomly inserts a point
+		}
+		float x_1, y_1, z_1, x_2, y_2, z_2, x_3, y_3, z_3;
+
+		auto itr = inliers.begin();		// points at beginning of inliers
+		x_1 = cloud->points[*itr].x;	//(cloud)->{points}[dereferences pointer -> INDEX into (cloud)].x value for {points}
+		y_1 = cloud->points[*itr].y;
+		z_1 = cloud->points[*itr].z;
+		itr++;
+		x_2 = cloud->points[*itr].x;
+		y_2 = cloud->points[*itr].y;
+		z_2 = cloud->points[*itr].z;
+		itr++;
+		x_3 = cloud->points[*itr].x;
+		y_3 = cloud->points[*itr].y;
+		z_3 = cloud->points[*itr].z;
+
+		// v_1 = < x2 − x1, y2 − y1, z2 − z1 >
+		float v_1_coor_1 = x2 - x1;
+		float v_1_coor_2 = y2 - y1;
+		float v_1_coor_3 = z2 - z1;
+
+		// v_2 = < x3 − x1, y3 − y1, z3 − z1 >
+		float v_2_coor_1 = x3 - x1;
+		float v_2_coor_2 = y3 - y1;
+		float v_2_coor_3 = z3 - z1;
+
+		// v1 X v2 = < (y2 − y1)(z3 − z1) - (z2 − z1)(y3 − y1), (z2 − z1)(x3 − x1) - (x2 − x1)(z3 − z1), (x2 − x1)(y3 − y1) - (y2 − y1)(x3 − x1) >
+		float i = (y2 - y1)(z3 - z1) - (z2 - z1)(y3 - y1);
+		float j = (z2 - z1)(x3 - x1) - (x2 - x1)(z3 - z1);
+		float k = (x2 - x1)(y3 - y1) - (y2 - y1)(x3 - x1);
+
+		// ix + jy + kz - (ix1 + jy1 + kz1) = 0
+		float A = i;
+		float B = j;
+		float C = k;
+		float D = - (ix1 + jy1 + kz1);
+
+		for (int index = 0; index < cloud->points.size(); index++)	// iterates through all points
+		{
+			if (inliers.count(index) > 0)	// checks if point isn't ALREADY accounted for
+			{
+				continue;
+			}
+			pcl::PointXYZ point = cloud->points[index];		// calculate distance & see if within threshold
+			float x_3 = point.x;	// show x value
+			float y_3 = point.y;	// show y value
+
+			float d = fabs(A*x_3 + B*y_3 + C) / sqrt(A*A + B*B);	// Distance:	d = |Ax + By + C| / sqrt(A^2 + B^2)
+
+			if (d <= distanceTol)
+			{
+				inliers.insert(index);
+			}
+		}
+		
+		if (inliers.size() > inliersResult.size())	// checks size of inliers
+		{
+			inliersResult = inliers;
+		}
+		
+	}
+	
+
+	return inliersResult;
+}
+
 int main ()
 {
 
