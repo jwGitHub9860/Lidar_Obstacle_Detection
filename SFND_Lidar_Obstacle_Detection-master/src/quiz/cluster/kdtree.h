db62bd91dabb87ac550f8ad31084703f4a4abb3e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-04-21 14:00:34 -0400

    Created "searchBinaryTree" and "search" functions

--- SFND_Lidar_Obstacle_Detection-master/src/quiz/cluster/kdtree.h
+++ SFND_Lidar_Obstacle_Detection-master/src/quiz/cluster/kdtree.h
@@ -66,14 +66,41 @@ struct KdTree
 		insertBinaryTree(&root, 0, point, id);		// Recursive Function - function that calls itself			NEED TO DEREFENCE &root TO CHANGE FROM (Node*) TO (Node **node)			(point, id) --> information needed to create BRAND NEW NODE
 	}
 
+	void searchBinaryTree(std::vector<float> target, Node* node, int depth, float distanceTol, std::vector<int>& ids)
+	{
+		if (node != NULL)
+		{
+			//	 (                        FIRST CONDITION - (x value ---> point[0])							)	 (					SECOND CONDITION - (y value ---> point[1])								)
+			if ( (node->point[0] >= (target[0] - distanceTol) && node->point[0] <= (target[0] + distanceTol)) && (node->point[1] >= (target[1] - distanceTol) && node->point[1] <= (target[1] - distanceTol)) )	// checks if that node is within that target box or not		target[0] - centered at target			CHECK IF IN TARGET BOX	--->  + or - distance tolerance
+			{
+				float distance = sqrt((node->point[0] - target[0]) * (node->point[0] - target[0]) + (node->point[1] - target[1]) * (node->point[1] - target[1]));	// find distance between (x, y) NODE & (x, y) TARGET
+				if (distance <= distanceTol)
+				{
+					ids.push_back(node->id);	// push back node ID (node->id) onto IDs (ids)
+				}
+			}
+			
+			//check across boundary			<--- (where you want to flow in tree  --->  left or right)
+			if ((target[depth % 2] - distanceTol) < node->point[depth % 2])		// [depth % 2] - determine to do X or Y comparison			LEFT ---> -distanceTol & less than x or y node value
+			{
+				searchBinaryTree(target, node->left, depth + 1, distanceTol, ids);
+			}
+			if ((target[depth % 2] + distanceTol) < node->point[depth % 2])		// [depth % 2] - determine to do X or Y comparison			RIGHT ---> +distanceTol & greater than x or y node value
+			{
+				searchBinaryTree(target, node->right, depth + 1, distanceTol, ids);
+			}
+			
+		}
+		
+	}
+
 	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
+	std::vector<int> search(std::vector<float> target, float distanceTol)		// target - representing suspected flow (x & y)			distanceTol (distance tolerance) --> which points in tree are within distance tolerance of target point
 	{
 		std::vector<int> ids;
+		searchBinaryTree(target, root, 0, distanceTol, ids);	// pass in target point, at start of tree (root), root depth (0), distanceTol, ids
 		return ids;
 	}
-	
-
 };
 
 
